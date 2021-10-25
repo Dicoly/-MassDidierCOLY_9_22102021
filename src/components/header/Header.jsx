aa4b05aa3de0dd689a393eb816befb42396642b3
Mass Didier Coly <mass-didier.coly@expleogroup.com>
2021-10-25 10:40:50 +0200

    Add Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="headerContent">
+            <img src={logo} alt="logo"/>
+            <nav className="headerNav">
+                <a className="headerNavLink" href="#Accueil">Accueil</a>
+                <a className="headerNavLink" href="#Profil">Profil</a>
+                <a className="headerNavLink" href="#Réglage">Réglage</a>
+                <a className="headerNavLink" href="#Communauté">Communauté</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
